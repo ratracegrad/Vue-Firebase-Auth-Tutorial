6015ae3c40ed5951f87b5b366d4cdfa6f6ab030e
talhaouymed <talhaouycoding@gmail.com>
2021-03-30 21:16:19 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 Vue.config.productionTip = false;
 
 new Vue({
