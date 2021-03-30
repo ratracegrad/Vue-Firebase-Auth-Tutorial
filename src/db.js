4192c8136b7d0d1e47a75e19a86face419c800d6
talhaouymed <talhaouycoding@gmail.com>
2021-03-30 21:28:57 +0100

    default setting firebase

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 var config = {
-    apiKey: 'AIzaSyAirDcT8LcgK3QMZve6Ye0vn_9iD2mHVkU',
-    authDomain: 'vue-auth-e08a4.firebaseapp.com',
-    projectId: 'vue-auth-e08a4',
-    storageBucket: 'vue-auth-e08a4.appspot.com',
-    messagingSenderId: '312910611871',
-    appId: '1:312910611871:web:5d0e1806579d920bf1a9aa',
+    apiKey: 'YourCode',
+    authDomain: 'YourCode',
+    projectId: 'YourCode',
+    storageBucket: 'YourCode',
+    messagingSenderId: 'YourCode',
+    appId: 'YourCode',
 };
 // Initialize Firebase
 const db = firebase.initializeApp(config);
